c4c0366506397103cf90b8efd8764b0d42f4b20c
trstyq <trstyq@gmail.com>
2021-09-23 22:55:51 +0900

    [성진] 10페이지까지 구현 완료

--- src/com/kh/recipe/mainPage/controller/MainPageController.java
+++ src/com/kh/recipe/mainPage/controller/MainPageController.java
@@ -25,6 +25,10 @@ public class MainPageController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		List<Recipe> Recipes = new ArrayList<Recipe>();
+		Recipes = mainPageService.selectRecipeByDetail();
+		request.setAttribute("Recipes", Recipes);
+		
 		String[] uriArr = request.getRequestURI().split("/");
 		
 		switch(uriArr[uriArr.length-1]) {
@@ -34,29 +38,97 @@ public class MainPageController extends HttpServlet {
 		case "2":
 			mainPage2(request, response);
 			break;
+		case "3":
+			mainPage3(request, response);
+			break;
+		case "4":
+			mainPage4(request, response);
+			break;
+		case "5":
+			mainPage5(request, response);
+			break;
+		case "6":
+			mainPage6(request, response);
+			break;
+		case "7":
+			mainPage7(request, response);
+			break;
+		case "8":
+			mainPage8(request, response);
+			break;
+		case "9":
+			mainPage9(request, response);
+			break;
+		case "10":
+			mainPage10(request, response);
+			break;
 		default: throw new PageNotFoundException();
 		}
 		 
-
 	}
-
-
-	private void mainPage2(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Recipe> Recipes = new ArrayList<Recipe>();
-		Recipes = mainPageService.selectRecipeByDetail();
-		request.setAttribute("Recipes", Recipes);
-		request.setAttribute("pageNumber", 2);
+	
+	private void mainPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setAttribute("pageNumber", 1);
 		request.getRequestDispatcher("/mainPage").forward(request, response);	
 	}
 
-	private void mainPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Recipe> Recipes = new ArrayList<Recipe>();
-		Recipes = mainPageService.selectRecipeByDetail();
-		request.setAttribute("Recipes", Recipes);
-		request.setAttribute("pageNumber", 1);
+	private void mainPage2(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setAttribute("pageNumber", 2);
 		request.getRequestDispatcher("/mainPage").forward(request, response);
 		
 	}
+	
+	private void mainPage3(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setAttribute("pageNumber", 3);
+		request.getRequestDispatcher("/mainPage").forward(request, response);	
+		
+	}
+	
+	private void mainPage4(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setAttribute("pageNumber", 4);
+		request.getRequestDispatcher("/mainPage").forward(request, response);	
+		
+	}
+	
+	private void mainPage5(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setAttribute("pageNumber", 5);
+		request.getRequestDispatcher("/mainPage").forward(request, response);	
+		
+	}
+	
+	private void mainPage6(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setAttribute("pageNumber", 6);
+		request.getRequestDispatcher("/mainPage").forward(request, response);	
+		
+	}
+
+	private void mainPage7(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setAttribute("pageNumber", 7);
+		request.getRequestDispatcher("/mainPage").forward(request, response);	
+		
+	}
+
+	private void mainPage8(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setAttribute("pageNumber", 8);
+		request.getRequestDispatcher("/mainPage").forward(request, response);	
+		
+	}
+
+	private void mainPage9(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setAttribute("pageNumber", 9);
+		request.getRequestDispatcher("/mainPage").forward(request, response);	
+		
+	}
+
+	private void mainPage10(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setAttribute("pageNumber", 10);
+		request.getRequestDispatcher("/mainPage").forward(request, response);	
+		
+	}
+
+	
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
