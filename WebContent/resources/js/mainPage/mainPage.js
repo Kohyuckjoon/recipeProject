cd453cf85e52830b3a4113c0c9b631f5a772287f
trstyq <trstyq@gmail.com>
2021-09-28 05:30:41 +0900

    [성진] :9090/favicon.ico:1 404 에러 수정

--- WebContent/resources/js/mainPage/mainPage.js
+++ WebContent/resources/js/mainPage/mainPage.js
@@ -1,6 +1,57 @@
-((pointer) => {
+(() => {
+	
+		let numberForPaging = (totalPage, viewPageNumbers, currentPage) => {
+		
+		this.totalPage = totalPage;
+		// 1000/36 = 27.7
+		this.currentPage = currentPage;
+		this.viewPageNumbers = viewPageNumbers;
+		let remainder = currentPage % viewPageNumbers;
+		let firstPagerNum = currentPage - remainder + 1;
+		let lastPagerNum = currentPage - remainder + viewPageNumbers;
+		
+		if(remainder == 0){
+			firstPagerNum -= 10;
+			lastPagerNum -= 10;
+		}
+		
+		if(this.totalPage < lastPagerNum ){
+			lastPagerNum = this.totalPage; 
+		}
+		
+		return{
+			totalPage,
+			currentPage,
+			viewPageNumbers,
+			firstPagerNum,
+			lastPagerNum
+		}
+		
+	}
+	
+	let paging = null;
+	let sPN = parseInt(sessionStorage.getItem('sessionPageNumber'));
 	
 	
+	if(isNaN(sPN) == true) {
+		paging = numberForPaging(28, 10, 1)
+	} else {
+		paging = numberForPaging(28, 10, sPN)
+	}
+	
+	for(let i = paging.firstPagerNum; i < paging.lastPagerNum + 1; i++){
+		let pageNumber = document.createElement('td');
+		pageNumber.classList.add('page-' + i);
+		pageNumber.append(i);
+		document.querySelector('.num').before(pageNumber);
+		
+		document.querySelector('.page-' + i).addEventListener('click',() =>{
+			sessionStorage.setItem('sessionPageNumber', i );
+			location.href="/mainPage/clickedPage";
+		});
+		
+		
+	}
 
 	
 })();
