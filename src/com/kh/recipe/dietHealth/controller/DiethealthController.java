541ce39d50ff8c2b1c6920d5013d7b3ccbec3da5
akhj123akhj <akhj123akhjK@gmail.com>
2021-09-30 10:50:21 +0900

    [고혁준] Controller /-> commit test 입니다(충돌문제)

--- src/com/kh/recipe/dietHealth/controller/DiethealthController.java
+++ src/com/kh/recipe/dietHealth/controller/DiethealthController.java
@@ -25,16 +25,34 @@ public class DiethealthController extends HttpServlet {
 		String[] uriArr = request.getRequestURI().split("/");
 		
 		switch (uriArr[uriArr.length-1]) {
-		case "searchResult":
+		case "searchResult": //검색결과
 			searchResult(request,response);
 			break;
+			
+		case "checkBox": //체크박스
+			checkBox(request,response);
+			break;
+			
+			
+			
+		
 
 		default: throw new PageNotFoundException();
 		}
 	}
 	
 
+	private void checkBox(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 	private void searchResult(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//체크박스 입력을 받아서,
+		//필요한 칼로리, 나트륨, 지방, 단백질을 입력 받아서
+		//sql 디벨로퍼에 있는 정보중에 부합하는것들을 
 		request.getRequestDispatcher("/dietHealth/searchResult").forward(request, response);
 	}
 
