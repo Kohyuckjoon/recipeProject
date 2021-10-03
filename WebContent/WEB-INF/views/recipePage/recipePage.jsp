aeee1b4164b0eda2f4b86d57a4ff69d19b34b9a2
trstyq <trstyq@gmail.com>
2021-10-03 22:05:22 +0900

    [성진] http://localhost:9090/recipePage/recipePage. 레시피 외형 구현

--- WebContent/WEB-INF/views/recipePage/recipePage.jsp
+++ WebContent/WEB-INF/views/recipePage/recipePage.jsp
@@ -1,192 +1,111 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <%@ include file="/WEB-INF/views/include/head.jsp"%>
 <link href="../resources/css/recipePage/recipePage.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-
-.top{
-	height: 100px;
-}
-
-.recipe_page{
-	width: 80vw;
-	display: flex;
-	flex-wrap: wrap;
-	background-color: white;
-	padding: 7vw;
-}
-
-.food_img{
-	display: inline-block;
-	width: 37vw;
-	height: 37vw;
-	background-color: silver;
-}
-
-.container{
-	display: flex;
-	flex-direction: column;
-	width: 31vw;
-	margin: 0 1vw 0 5vw;
-}
-
-.food_category{
-	color: orange;
-	height: 1.7vw;
-}
-
-.food_name{
-	height: 4vw;
-	font-weight: bold;
-}
-
-.nutrition-fact_tit{
-	height: 3vw;
-}
-
-.nutrition-fact{
-	height: 10vw;
-}
-
-.ingredients_tit{
-	height: 3vw;
-}
-
-.ingredients{
-
-}
-
-.scrap{
-	height: 5vw;
-}
-
-.scrap i{
-	font-size: 5vw;
-}
-
-.recipe_tit{
-	width: 100%;
-	font-size: 2vw;
-	margin: 9vw 0 3vw 3vw;
-}
-
-.recipe{
-	width: 100%;
-	padding: 3vw;
-	margin-bottom: 13vw
-}
-
-.reply_list{
-	width: 100%;
-	position:relative;
-    left: 50%;
-    transform: translateX(-50%);
-}
-
-.reply_list table{
-	width:100%;
-	
-}
-
-.reply_list tr {
-	border-bottom :1px solid #999;
-}
-
-.reply_list th,
-.reply_list td{
-	text-align: center;
-}
-
-.reply_list tbody tr td:nth-child(2){
-	text-align: left;
-}
-
-.replt_form{
-	margin-top: 3vw;
-}
-
-#form{
-	width: 70vw;
-	height: 7vw;
-}
-
-#submit{
-	width: 8.5vw;
-	height: 7vw;
-}
-
-</style>
 </head>
 
 <body>
 
-<%@ include file="/WEB-INF/views/include/header.jsp" %>
-<div class="top"></div>
+	<%@ include file="/WEB-INF/views/include/header.jsp"%>
 
-<div class="recipe_page">
+	<div class="recipe_page">
 
-	<div class="food_img"></div>
-	
-	<div class="container">
-		<div class="food_category">-밥-</div>
-		<div class="food_name">칼륨 듬뿍 고구마죽</div>
+		<img class="food_img" src="http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00017_1.png"></img>
+
+		<div class="container">
+			<div class="food_category">-밥-</div>
+			<div class="food_name">칼륨 듬뿍 고구마죽</div>
+
+			<div class="nutrition_fact">
+				<div class = "nutrition_fact_title">영양성분</div>
+				<div class = "nutrition_fact_detail">
+					<div class = "calory">&#x25C9 열량 : 1</div>
+					<div class = "carbohydrate">&#x25C9 탄수화물 : 2</div>
+					<div class = "protein">&#x25C9 단백질 : 3</div>
+					<div class = "fat">&#x25C9 지방 : 4</div>
+					<div class = "natrium ">&#x25C9 나트륨 : 5</div>
+				</div>
+			</div>
+
+			<div class="ingredients">
+				<div class="ingredients_title">재료</div>
+				<div class="ingredients_detail">
+					<div class="ingredients_detail_1">&#x25C9 고구마죽 고구마 100g(2/3개)</div>
+					<div class="ingredients_detail_2">&#x25C9 설탕 2g(1/3작은술)</div>
+					<div class="ingredients_detail_3">&#x25C9 찹쌀가루 3g(2/3작은술)</div>
+					<div class="ingredients_detail_4">&#x25C9 물 200ml(1컵)</div>
+					<div class="ingredients_detail_5">&#x25C9 잣 8g(8알)</div>
+				</div>
+			</div>
+			
+		</div>
+
+		<div class="scrap">
+			<i class="far fa-bookmark"></i>
+		</div>
 		
-		<div class="nutrition-fact_tit">영양성분</div>
-		<div class="nutrition-fact"></div>
+		<div class="share"></div>
+
+		<div class="cooking_method">
+			<img class="cooking_method_img" src="http://www.foodsafetykorea.go.kr/uploadimg/cook/20_00017_1.png"></img>
+			<div class="cooking_method_detail">1. 고구마는 깨끗이 씻어서 껍질을 벗기고 4cm 정도로 잘라준다.a</div>
+			<img class="cooking_method_img" src="http://www.foodsafetykorea.go.kr/uploadimg/cook/20_00017_2.png"></img>
+			<div class="cooking_method_detail">2. 찜기에 고구마를 넣고 20~30분 정도 삶아 주고, 블렌더나 체를 이용하여 잘 으깨어 곱게 만든다.b</div>
+			<img class="cooking_method_img" src="http://www.foodsafetykorea.go.kr/uploadimg/cook/20_00017_3.png"></img>
+			<div class="cooking_method_detail">3. 고구마와 물을 섞어 끓이면서 찹쌀가루로 농도를 맞추고 설탕을 넣어 맛을 낸다.c</div>
+			<div class="cooking_method_detail">4. 잣을 팬에 노릇하게 볶아 다져서 고구마 죽에 섞는다. 기호에 따라 고구마를 튀겨 얹어 먹어도 좋다.</div>
+		</div>
 		
-		<div class="ingredients_tit">재료</div>
-		<div class="ingredients"></div>
-	</div>
-	
-	<div class="scrap">
-		<i class="far fa-bookmark"></i>
-	</div>
-	<div class="share"></div>
-	
-	<div class="recipe_tit">조리 방법</div>
-	<div class="recipe">
-		<pre>1.
-2.
-3.
-4.
-5.</pre>
-	</div>
-	
-	<div class="reply_list">
-		<table>
-			<thead>
-				<tr>
-					<th style="width:10%;">No.</th>
-					<th style="width:60%;">내용</th>
-					<th style="width:15%;">작성자</th>
-					<th style="width:15%;">작성일</th>
-				</tr>
-			</thead>
-			<tbody>
-				<c:forEach items="${reviews}" var="review">
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		<div class="reply_list">
+			<table>
+				<thead>
 					<tr>
-						<td>${review.reviewNo}</td>
-						<td>${review.reviewContents}</td>
-						<td>${review.userId}</td>
-						<td>${review.reviewDate}</td>
+						<th style="width: 10%;">No.</th>
+						<th style="width: 60%;">내용</th>
+						<th style="width: 15%;">작성자</th>
+						<th style="width: 15%;">작성일</th>
 					</tr>
-				</c:forEach>
-			</tbody>
-		</table>
-	</div>
-	
-	<div class="replt_form">
-		<form>
-			<input id="form" type="text" placeholder="댓글 입력">
-			<input id="submit" type="submit" value="댓글 등록">
-		</form>
-	</div>
-	
-</div>
+				</thead>
+				<tbody>
+					<c:forEach items="${reviews}" var="review">
+						<tr>
+							<td>${review.reviewNo}</td>
+							<td>${review.reviewContents}</td>
+							<td>${review.userId}</td>
+							<td>${review.reviewDate}</td>
+						</tr>
+					</c:forEach>
+				</tbody>
+			</table>
+		</div>
 
-<%@ include file="/WEB-INF/views/include/foot.jsp" %>
+		<div class="replt_form">
+			<form>
+				<input id="form" type="text" placeholder="댓글 입력"> <input id="submit" type="submit" value="댓글 등록">
+			</form>
+		</div>
+
+	</div>
+
+	<%@ include file="/WEB-INF/views/include/foot.jsp"%>
 </body>
 </html>
--- src/com/kh/recipe/recipePage/model/dto/Recipe.java
+++ src/com/kh/recipe/recipePage/model/dto/Recipe.java
@@ -48,202 +48,446 @@ public class Recipe {
 	private String manual19;
 	private String manual20;
 	
+	private String manualImg01;
+	private String manualImg02;
+	private String manualImg03;
+	private String manualImg04;
+	private String manualImg05;
+	private String manualImg06;
+	private String manualImg07;
+	private String manualImg08;
+	private String manualImg09;
+	private String manualImg10;
+	private String manualImg11;
+	private String manualImg12;
+	private String manualImg13;
+	private String manualImg14;
+	private String manualImg15;
+	private String manualImg16;
+	private String manualImg17;
+	private String manualImg18;
+	private String manualImg19;
+	private String manualImg20;
+
 	public Recipe() {
 	}
-	
+
 	public int getRcpSeq() {
 		return rcpSeq;
 	}
+
 	public void setRcpSeq(int rcpSeq) {
 		this.rcpSeq = rcpSeq;
 	}
+
 	public String getRcpNm() {
 		return rcpNm;
 	}
+
 	public void setRcpNm(String rcpNm) {
 		this.rcpNm = rcpNm;
 	}
+
 	public String getRcpWay2() {
 		return rcpWay2;
 	}
+
 	public void setRcpWay2(String rcpWay2) {
 		this.rcpWay2 = rcpWay2;
 	}
+
 	public String getRcpPat2() {
 		return rcpPat2;
 	}
+
 	public void setRcpPat2(String rcpPat2) {
 		this.rcpPat2 = rcpPat2;
 	}
+
 	public int getInfoEng() {
 		return infoEng;
 	}
+
 	public void setInfoEng(int infoEng) {
 		this.infoEng = infoEng;
 	}
+
 	public int getInfoCar() {
 		return infoCar;
 	}
+
 	public void setInfoCar(int infoCar) {
 		this.infoCar = infoCar;
 	}
+
 	public int getInfoPro() {
 		return infoPro;
 	}
+
 	public void setInfoPro(int infoPro) {
 		this.infoPro = infoPro;
 	}
+
 	public int getInfoFat() {
 		return infoFat;
 	}
+
 	public void setInfoFat(int infoFat) {
 		this.infoFat = infoFat;
 	}
+
 	public int getInfoNa() {
 		return infoNa;
 	}
+
 	public void setInfoNa(int infoNa) {
 		this.infoNa = infoNa;
 	}
+
 	public String getAttFileNoMain() {
 		return attFileNoMain;
 	}
+
 	public void setAttFileNoMain(String attFileNoMain) {
 		this.attFileNoMain = attFileNoMain;
 	}
+
 	public String getAttFileNoMk() {
 		return attFileNoMk;
 	}
+
 	public void setAttFileNoMk(String attFileNoMk) {
 		this.attFileNoMk = attFileNoMk;
 	}
+
 	public String getRcpPartsDtls() {
 		return rcpPartsDtls;
 	}
+
 	public void setRcpPartsDtls(String rcpPartsDtls) {
 		this.rcpPartsDtls = rcpPartsDtls;
 	}
+
 	public String getManual1() {
 		return manual1;
 	}
+
 	public void setManual1(String manual1) {
 		this.manual1 = manual1;
 	}
+
 	public String getManual2() {
 		return manual2;
 	}
+
 	public void setManual2(String manual2) {
 		this.manual2 = manual2;
 	}
+
 	public String getManual3() {
 		return manual3;
 	}
+
 	public void setManual3(String manual3) {
 		this.manual3 = manual3;
 	}
+
 	public String getManual4() {
 		return manual4;
 	}
+
 	public void setManual4(String manual4) {
 		this.manual4 = manual4;
 	}
+
 	public String getManual5() {
 		return manual5;
 	}
+
 	public void setManual5(String manual5) {
 		this.manual5 = manual5;
 	}
+
 	public String getManual6() {
 		return manual6;
 	}
+
 	public void setManual6(String manual6) {
 		this.manual6 = manual6;
 	}
+
 	public String getManual7() {
 		return manual7;
 	}
+
 	public void setManual7(String manual7) {
 		this.manual7 = manual7;
 	}
+
 	public String getManual8() {
 		return manual8;
 	}
+
 	public void setManual8(String manual8) {
 		this.manual8 = manual8;
 	}
+
 	public String getManual9() {
 		return manual9;
 	}
+
 	public void setManual9(String manual9) {
 		this.manual9 = manual9;
 	}
+
 	public String getManual10() {
 		return manual10;
 	}
+
 	public void setManual10(String manual10) {
 		this.manual10 = manual10;
 	}
+
 	public String getManual11() {
 		return manual11;
 	}
+
 	public void setManual11(String manual11) {
 		this.manual11 = manual11;
 	}
+
 	public String getManual12() {
 		return manual12;
 	}
+
 	public void setManual12(String manual12) {
 		this.manual12 = manual12;
 	}
+
 	public String getManual13() {
 		return manual13;
 	}
+
 	public void setManual13(String manual13) {
 		this.manual13 = manual13;
 	}
+
 	public String getManual14() {
 		return manual14;
 	}
+
 	public void setManual14(String manual14) {
 		this.manual14 = manual14;
 	}
+
 	public String getManual15() {
 		return manual15;
 	}
+
 	public void setManual15(String manual15) {
 		this.manual15 = manual15;
 	}
+
 	public String getManual16() {
 		return manual16;
 	}
+
 	public void setManual16(String manual16) {
 		this.manual16 = manual16;
 	}
+
 	public String getManual17() {
 		return manual17;
 	}
+
 	public void setManual17(String manual17) {
 		this.manual17 = manual17;
 	}
+
 	public String getManual18() {
 		return manual18;
 	}
+
 	public void setManual18(String manual18) {
 		this.manual18 = manual18;
 	}
+
 	public String getManual19() {
 		return manual19;
 	}
+
 	public void setManual19(String manual19) {
 		this.manual19 = manual19;
 	}
+
 	public String getManual20() {
 		return manual20;
 	}
+
 	public void setManual20(String manual20) {
 		this.manual20 = manual20;
 	}
-	
+
+	public String getManualImg01() {
+		return manualImg01;
+	}
+
+	public void setManualImg01(String manualImg01) {
+		this.manualImg01 = manualImg01;
+	}
+
+	public String getManualImg02() {
+		return manualImg02;
+	}
+
+	public void setManualImg02(String manualImg02) {
+		this.manualImg02 = manualImg02;
+	}
+
+	public String getManualImg03() {
+		return manualImg03;
+	}
+
+	public void setManualImg03(String manualImg03) {
+		this.manualImg03 = manualImg03;
+	}
+
+	public String getManualImg04() {
+		return manualImg04;
+	}
+
+	public void setManualImg04(String manualImg04) {
+		this.manualImg04 = manualImg04;
+	}
+
+	public String getManualImg05() {
+		return manualImg05;
+	}
+
+	public void setManualImg05(String manualImg05) {
+		this.manualImg05 = manualImg05;
+	}
+
+	public String getManualImg06() {
+		return manualImg06;
+	}
+
+	public void setManualImg06(String manualImg06) {
+		this.manualImg06 = manualImg06;
+	}
+
+	public String getManualImg07() {
+		return manualImg07;
+	}
+
+	public void setManualImg07(String manualImg07) {
+		this.manualImg07 = manualImg07;
+	}
+
+	public String getManualImg08() {
+		return manualImg08;
+	}
+
+	public void setManualImg08(String manualImg08) {
+		this.manualImg08 = manualImg08;
+	}
+
+	public String getManualImg09() {
+		return manualImg09;
+	}
+
+	public void setManualImg09(String manualImg09) {
+		this.manualImg09 = manualImg09;
+	}
+
+	public String getManualImg10() {
+		return manualImg10;
+	}
+
+	public void setManualImg10(String manualImg10) {
+		this.manualImg10 = manualImg10;
+	}
+
+	public String getManualImg11() {
+		return manualImg11;
+	}
+
+	public void setManualImg11(String manualImg11) {
+		this.manualImg11 = manualImg11;
+	}
+
+	public String getManualImg12() {
+		return manualImg12;
+	}
+
+	public void setManualImg12(String manualImg12) {
+		this.manualImg12 = manualImg12;
+	}
+
+	public String getManualImg13() {
+		return manualImg13;
+	}
+
+	public void setManualImg13(String manualImg13) {
+		this.manualImg13 = manualImg13;
+	}
+
+	public String getManualImg14() {
+		return manualImg14;
+	}
+
+	public void setManualImg14(String manualImg14) {
+		this.manualImg14 = manualImg14;
+	}
+
+	public String getManualImg15() {
+		return manualImg15;
+	}
+
+	public void setManualImg15(String manualImg15) {
+		this.manualImg15 = manualImg15;
+	}
+
+	public String getManualImg16() {
+		return manualImg16;
+	}
+
+	public void setManualImg16(String manualImg16) {
+		this.manualImg16 = manualImg16;
+	}
+
+	public String getManualImg17() {
+		return manualImg17;
+	}
+
+	public void setManualImg17(String manualImg17) {
+		this.manualImg17 = manualImg17;
+	}
+
+	public String getManualImg18() {
+		return manualImg18;
+	}
+
+	public void setManualImg18(String manualImg18) {
+		this.manualImg18 = manualImg18;
+	}
+
+	public String getManualImg19() {
+		return manualImg19;
+	}
+
+	public void setManualImg19(String manualImg19) {
+		this.manualImg19 = manualImg19;
+	}
+
+	public String getManualImg20() {
+		return manualImg20;
+	}
+
+	public void setManualImg20(String manualImg20) {
+		this.manualImg20 = manualImg20;
+	}
+
 	@Override
 	public String toString() {
 		return "Recipe [rcpSeq=" + rcpSeq + ", rcpNm=" + rcpNm + ", rcpWay2=" + rcpWay2 + ", rcpPat2=" + rcpPat2
@@ -254,8 +498,17 @@ public class Recipe {
 				+ manual7 + ", manual8=" + manual8 + ", manual9=" + manual9 + ", manual10=" + manual10 + ", manual11="
 				+ manual11 + ", manual12=" + manual12 + ", manual13=" + manual13 + ", manual14=" + manual14
 				+ ", manual15=" + manual15 + ", manual16=" + manual16 + ", manual17=" + manual17 + ", manual18="
-				+ manual18 + ", manual19=" + manual19 + ", manual20=" + manual20 + "]";
+				+ manual18 + ", manual19=" + manual19 + ", manual20=" + manual20 + ", manualImg01=" + manualImg01
+				+ ", manualImg02=" + manualImg02 + ", manualImg03=" + manualImg03 + ", manualImg04=" + manualImg04
+				+ ", manualImg05=" + manualImg05 + ", manualImg06=" + manualImg06 + ", manualImg07=" + manualImg07
+				+ ", manualImg08=" + manualImg08 + ", manualImg09=" + manualImg09 + ", manualImg10=" + manualImg10
+				+ ", manualImg11=" + manualImg11 + ", manualImg12=" + manualImg12 + ", manualImg13=" + manualImg13
+				+ ", manualImg14=" + manualImg14 + ", manualImg15=" + manualImg15 + ", manualImg16=" + manualImg16
+				+ ", manualImg17=" + manualImg17 + ", manualImg18=" + manualImg18 + ", manualImg19=" + manualImg19
+				+ ", manualImg20=" + manualImg20 + "]";
 	}
+	
+	
 	
 	
 	
